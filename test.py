d2b8d8438afc107f3c6bb422f552d7968eefe8b1
loncat <me@lcat.dev>
2020-05-10 07:17:20 +0800

    Just a modified test

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ import kyros
 
 async def main():
     whatsapp = await kyros.Whatsapp.create()
-    qr_data, waiter = await whatsapp.qr_login()
+    qr_data, timeout = await whatsapp.qr_login()
     qr = pyqrcode.create(qr_data)
     print(qr.terminal(quiet_zone=1))
-    await waiter
+    await timeout
 
 
 asyncio.get_event_loop().run_until_complete(main())
