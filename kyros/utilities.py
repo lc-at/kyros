47ca3b741454484e482841b4b5bb81d7622a05e0
loncat <me@lcat.dev>
2020-05-10 07:16:51 +0800

    generate_client_id() returns string by default

--- kyros/utilities.py
+++ kyros/utilities.py
@@ -7,5 +7,5 @@ def generate_message_tag() -> str:
     return str(time.time())
 
 
-def generate_client_id() -> bytes:
-    return base64.b64encode(os.urandom(16))
+def generate_client_id() -> str:
+    return base64.b64encode(os.urandom(16)).decode()
