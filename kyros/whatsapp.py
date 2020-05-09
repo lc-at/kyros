3a7cc3a6bbfd6a32e20977fb25ff50670f734ed3
loncat <me@lcat.dev>
2020-05-10 07:17:35 +0800

    Key generation process done

--- kyros/whatsapp.py
+++ kyros/whatsapp.py
@@ -5,7 +5,7 @@ import json
 import donna25519
 import websockets
 
-from . import constants, utilities
+from . import session, constants, crypto, utilities
 
 
 class Whatsapp:
@@ -16,16 +16,10 @@ class Whatsapp:
         return whatsapp
 
     def __init__(self):
-        self.client_id = utilities.generate_client_id()
-        self.server_id = None
-
-        self.client_token = None
-        self.server_token = None
-
-        self.secret = None
-
-        self.private_key = donna25519.PrivateKey()
-        self.public_key = self.private_key.get_public()
+        self.session = session.WhatsappSession()
+        self.session.client_id = utilities.generate_client_id()
+        self.session.private_key = donna25519.PrivateKey()
+        self.session.public_key = self.session.private_key.get_public()
 
     async def connect_ws(self):
         self.ws = await websockets.connect(constants.WEBSOCKET_URI,
@@ -45,24 +39,51 @@ class Whatsapp:
             self.encode_ws_message([
                 "admin", "init", constants.CLIENT_VERSION,
                 [constants.CLIENT_LONG_DESC, constants.CLIENT_SHORT_DESC],
-                self.client_id.decode(), True
+                self.session.client_id, True
             ])["data"])
+
         message = self.decode_ws_message(await self.ws.recv())
         if message["data"]["status"] != 200:
             raise Exception(f"login failed, message: {message}")
-        self.server_id = message["data"]["ref"]
+
+        self.session.server_id = message["data"]["ref"]
 
     async def qr_login(self):
         await self.send_init()
 
         async def wait_qr_scan():
             message = self.decode_ws_message(await self.ws.recv())
-            self.secret = message["data"][1]['secret']
+            connection_data = message["data"][1]
+
+            self.session.secret = base64.b64decode(
+                connection_data["secret"].encode())
+            self.session.server_token = connection_data["serverToken"]
+            self.session.client_token = connection_data["clientToken"]
+            self.session.browser_token = connection_data["browserToken"]
+
+            self.session.shared_secret = self.session.private_key.do_exchange(
+                donna25519.PublicKey(self.session.secret[:32]))
+            self.session.shared_secret_expanded = crypto.hkdf_expand(
+                self.session.shared_secret, 80)
+
+            if not crypto.validate_secrets(
+                    self.session.secret, self.session.shared_secret_expanded):
+                raise Exception("HMAC validation failed")
+
+            self.session.keys_encrypted = self.session.shared_secret_expanded[
+                64:] + self.session.secret[64:]
+            self.session.keys_decrypted = crypto.aes_decrypt(
+                self.session.shared_secret_expanded[:32],
+                self.session.keys_encrypted)
+
+            self.session.enc_key = self.session.keys_decrypted[:32]
+            self.session.mac_key = self.session.keys_decrypted[32:64]
+            print(self.session.enc_key, self.session.mac_key)
 
         qr_fragments = [
-            self.server_id,
-            base64.b64encode(self.public_key.public).decode(),
-            self.client_id.decode()
+            self.session.server_id,
+            base64.b64encode(self.session.public_key.public).decode(),
+            self.session.client_id
         ]
         qr = ",".join(qr_fragments)
 
