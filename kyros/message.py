f42d242056f46f9bc24aeafb4bedbdf1cfda3fc9
loncat <me@lcat.dev>
2020-05-23 07:20:11 +0800

    Future class to handle message

---
+++ kyros/message.py
@@ -0,0 +1,12 @@
+class MessageHandler:
+    def handle_text_message(self):
+        pass
+
+    def handle_image_message(self):
+        pass
+
+    def handle_video_message(self):
+        pass
+
+    def handle_json_message(self):
+        pass
