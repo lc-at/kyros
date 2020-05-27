eee8f3562893368e8b0e7e035a47b1bfbe6df1ab
loncat <me@lcat.dev>
2020-05-27 21:23:56 +0800

    New status code exc

--- kyros/exceptions.py
+++ kyros/exceptions.py
@@ -1,6 +1,13 @@
 class HMACValidationError(Exception):
-    message = "something when wrong when validating secrets"
+    message = "validation failed"
 
 
 class LoginError(Exception):
     pass
+
+
+class StatusCodeError(Exception):
+    def __init__(self, code):
+        self.code = code
+        message = f'Unexpected status code: {code}'
+        super().__init__(message)
