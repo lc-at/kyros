681275145b9a535bc312b5887ab6e7c050694849
loncat <me@lcat.dev>
2020-05-27 21:23:47 +0800

    New damn test

--- test.py
+++ test.py
@@ -1,16 +1,29 @@
 import asyncio
+import logging
+import sys
 
 import pyqrcode
 
 import kyros
 
+logging.basicConfig(level=logging.DEBUG)
 
-async def main():
+
+async def amain():
     whatsapp = await kyros.Client.create()
-    qr_data, timeout = await whatsapp.qr_login()
-    qr = pyqrcode.create(qr_data)
-    print(qr.terminal(quiet_zone=1))
-    await timeout
+    qr_data, scanned = await whatsapp.qr_login()
+    qr_code = pyqrcode.create(qr_data)
+    print(qr_code.terminal(quiet_zone=1))
+    try:
+        await scanned
+    except asyncio.TimeoutError:
+        await whatsapp.shutdown()
+        return
+    await whatsapp.logout()
 
 
-asyncio.get_event_loop().run_until_complete(main())
+if __name__ == "__main__":
+    try:
+        asyncio.run(amain())
+    except KeyboardInterrupt:
+        sys.exit()
