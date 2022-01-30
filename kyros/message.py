80a7082db202c94c23e935086ca94abe615fe962
Reinier Millo <millo@ikoabo.com>
2022-01-29 22:39:42 -0500

    Add message data handler
    * Add callback function
    * TODO Identify message types to call handlers
    
    Signed-off-by: Reinier Millo <millo@ikoabo.com>

--- kyros/message.py
+++ kyros/message.py
@@ -1,5 +1,31 @@
+import logging
+from .bin_reader import read_binary
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
 class MessageHandler:
-    """Future class. To be implemented soon."""
+
+    def __init__(self, on_message=None) -> None:
+        """Initialize the message handler setting the callback function"""
+        self.on_message = on_message
+
+    def handle_message(self, message):
+        """Decode binary message"""
+        if message.data != "":
+            try:
+                msg_data = read_binary(message.data, True)
+                if self.on_message is not None:
+                    self.on_message(msg_data)
+
+                """Message must be identified by type to call related handler"""            
+
+                logger.debug("Received message: %s", msg_data)
+            except Exception as exc:
+                logger.error(
+                    "There were an exception error processing received message: %s", str(exc))
+        else:
+            logger.error("Unknown empty message: %s", message)
+
     def handle_text_message(self):
         pass
 
